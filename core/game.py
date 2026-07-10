44adf6e34f6a1fc48653a87890f370993ee9ec59
Tomas <tomymanijacaballero@gmail.com>
2026-07-10 15:45:24 -0300

    implementacion de los comandos y su teclas

--- core/game.py
+++ core/game.py
@@ -1,15 +1,72 @@
 import pygame
 import config
+from entities.player import Player
+
+from patterns.command.player.move_left import MoveLeftCommand
+from patterns.command.player.move_right import MoveRightCommand
+from patterns.command.player.move_jump import JumpCommand
+
+
 class Game:
+
     def __init__(self):
+
         pygame.init()
-        #creamos la ventana usando las variables de la configuración
-        self.screen = pygame.display.set_mode((config.ANCHO,config.ALTO))
-        #titulo de la ventana
+
+        self.screen = pygame.display.set_mode((config.ANCHO, config.ALTO))
+
         pygame.display.set_caption(config.TITULO)
 
-        #reloj del juego
-        self.clock=pygame.time.Clock()
+        self.clock = pygame.time.Clock()
+
+        self.running = True
+
+        # Jugador
+        self.player = Player()
+
+        # Diccionario de comandos
+        self.commands = {
+
+            pygame.K_a: MoveLeftCommand(self.player),
+
+            pygame.K_d: MoveRightCommand(self.player),
+
+            pygame.K_SPACE: JumpCommand(self.player)
+        }
+
+    def handle_events(self):
+
+    # Eventos que ocurren una sola vez
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                self.running = False
+
+            elif event.type == pygame.KEYDOWN:
+
+                if event.key == pygame.K_SPACE:
+                    self.commands[event.key].execute()
+
+    # Movimiento continuo
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.commands[pygame.K_a].execute()
+
+        if keys[pygame.K_d]:
+            self.commands[pygame.K_d].execute()
+
+    def update(self):
+        self.player.update()
+
+    def draw(self):
+
+        self.screen.fill((30,30,30))
+
+        self.player.draw(self.screen)
+
+        pygame.display.flip()
+
     def run(self):
         running = True
         while running: 
@@ -17,8 +74,17 @@ class Game:
                 if event.type == pygame.QUIT:
                     running = False
             pygame.display.flip()
+            self.handle_events()
+            self.update()
+            self.draw()
             self.clock.tick(config.FPS)
+            
         pygame.quit()
+    
+
+    
+
+
    
 
 

787981c74e866ffd3bd361ae7f2619cd6ce62830
diego <caipediego541@gmail.com>
2026-07-10 22:23:32 -0300

    Agrega acciones y animaciones básicas al jugador

--- entities/player.py
+++ entities/player.py
@@ -3,7 +3,7 @@ import pygame
 class Player:
 
     def __init__(self):
-
+        
         self.x = 350
         self.y = 450
 
@@ -16,6 +16,8 @@ class Player:
         self.gravity = 1
         self.jumping = False
         self.ground = 450
+        self.action = "idle"
+        self.action_timer = 0
 
     def move_left(self):
         self.x -= self.speed
@@ -33,12 +35,65 @@ class Player:
             self.jumping = True
 
     def draw(self, screen):
-
-        pygame.draw.rect(
-            screen,
-            (255,0,0),
-            (self.x,self.y,self.width,self.height)
-        )
+        # Colores
+        skin = (255, 220, 177)
+        red = (255, 0, 0)
+        black = (0, 0, 0)
+        # Cabeza
+        head_size = 20
+        head_x = self.x + 15
+        head_y = self.y
+        # Cuerpo
+        body_x = self.x + 10
+        body_y = self.y + 20
+        body_width = 30
+        body_height = 40
+        # Brazos
+        left_arm_x = self.x
+        right_arm_x = self.x + 40
+        arm_y = self.y + 25
+        arm_width = 10
+        arm_height = 30
+        # Piernas
+        left_leg_x = self.x + 15
+        right_leg_x = self.x + 30
+        leg_y = self.y + 60
+        leg_width = 8
+        leg_height = 25
+        # Cabeza
+        pygame.draw.rect(screen, skin, (head_x, head_y, head_size, head_size))
+        # Cuerpo
+        pygame.draw.rect(screen, red, (body_x, body_y, body_width, body_height))
+        if self.action == "idle":
+            # Brazos
+            pygame.draw.rect(screen, black, (left_arm_x, arm_y, arm_width, arm_height))
+            pygame.draw.rect(screen, black, (right_arm_x, arm_y, arm_width, arm_height))
+            # Piernas
+            pygame.draw.rect(screen, black, (left_leg_x, leg_y, leg_width, leg_height))
+            pygame.draw.rect(screen, black, (right_leg_x, leg_y, leg_width, leg_height))
+        elif self.action == "punch":
+            # Brazo izquierdo normal
+            pygame.draw.rect(screen, black, (left_arm_x, arm_y, arm_width, arm_height))
+            # Brazo derecho extendido
+            pygame.draw.rect(screen, black, (body_x + body_width, arm_y + 5, 25, 8))
+            # Piernas
+            pygame.draw.rect(screen, black, (left_leg_x, leg_y, leg_width, leg_height))
+            pygame.draw.rect(screen, black, (right_leg_x, leg_y, leg_width, leg_height))
+        elif self.action == "kick":
+            # Brazos normales
+            pygame.draw.rect(screen, black, (left_arm_x, arm_y, arm_width, arm_height))
+            pygame.draw.rect(screen, black, (right_arm_x, arm_y, arm_width, arm_height))
+            # Pierna izquierda normal
+            pygame.draw.rect(screen, black, (left_leg_x, leg_y, leg_width, leg_height))
+            # Pierna derecha extendida
+            pygame.draw.rect(screen, black, (right_leg_x, leg_y + 5, 25, 8))
+        elif self.action == "block":
+            # Brazos delante del cuerpo
+            pygame.draw.rect(screen, black, (body_x - 5, arm_y + 5, 40, 8))
+            pygame.draw.rect(screen, black, (body_x - 5, arm_y + 15, 40, 8))
+            # Piernas
+            pygame.draw.rect(screen, black, (left_leg_x, leg_y, leg_width, leg_height))
+            pygame.draw.rect(screen, black, (right_leg_x, leg_y, leg_width, leg_height))
 
     def update(self):
 
@@ -51,3 +106,19 @@ class Player:
                 self.y = self.ground
                 self.velocity_y = 0
                 self.jumping = False
+        if self.action_timer > 0:
+            self.action_timer -= 1
+            if self.action_timer == 0:
+                self.action = "idle"
+
+    def punch(self):
+        self.action = "punch"
+        self.action_timer = 10
+    
+    def kick(self):
+        self.action = "kick"
+        self.action_timer = 10
+    
+    def block(self):
+        self.action = "block"
+        self.action_timer = 10

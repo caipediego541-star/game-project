addf15ea56dec43a959afaa13828d632949d25ea
diego <caipediego541@gmail.com>
2026-07-25 13:03:28 -0300

    ajustes menores

--- managers/state_manager.py
+++ managers/state_manager.py
@@ -1,29 +1,19 @@
 class StateManager:
 
     def __init__(self):
-<<<<<<< HEAD
-        self.current_state=None
-    def set_state(self,state):
-        """Cambia el estado actual"""
-        self.current_state = state
-
-        if hasattr(state, "start"):
-            state.start(state.game)
-    def handle_events(self,events):
-        """Envia los eventos al estado actual"""
-=======
         self.current_state = None
         self.previous_state = None
-
 
     def set_state(self, state):
         if self.current_state:
             self.previous_state = self.current_state
+
         self.current_state = state
-        if hasattr(
-            self.current_state,
-            "enter"
-        ):
+
+        if hasattr(self.current_state, "start"):
+            self.current_state.start(self.current_state.game)
+
+        if hasattr(self.current_state, "enter"):
             self.current_state.enter()
 
     def return_previous_state(self):
@@ -31,15 +21,12 @@ class StateManager:
             self.current_state = self.previous_state
 
     def handle_events(self, events):
->>>>>>> 07d51f75fbe74859957961a873a8b0ba5d65deb6
         if self.current_state:
             self.current_state.handle_events(events)
-
 
     def update(self):
         if self.current_state:
             self.current_state.update()
-
 
     def draw(self, screen):
         if self.current_state:
--- patterns/state/mainMenuState.py
+++ patterns/state/mainMenuState.py
@@ -1,7 +1,9 @@
 import pygame
+import config
 from patterns.state.stateBase import State
 from utils.helpers import scale_image
 from patterns.factory.button_factory import ButtonFactory
+
 
 
 class MainMenuState(State):
@@ -20,24 +22,25 @@ class MainMenuState(State):
         imagen_boton = self.game.resource_manager.get_image("boton_jugar")
         imagen_boton = pygame.transform.scale_by(imagen_boton,0.4)
         separacion = 10
-        y_jugar = 150
+        x_jugar = (config.ANCHO - imagen_boton.get_width()) // 2
+        y_jugar = 180
         y_opciones = y_jugar + imagen_boton.get_height() + separacion
-        y_salir = y_opciones + imagen_boton.get_height() + separacion
+        y_salir = y_opciones + imagen_boton.get_height() + separacion - 70
         self.play_button = ButtonFactory.create_button(
             image=imagen_boton,
-            x=260,
-        
-            y=80,
+            x=x_jugar,
+            y=y_jugar,
             action=self.start_game
         )
         self.buttons.append(self.play_button)
         #boton opciones
         imagen_opciones = self.game.resource_manager.get_image("boton_opciones")
         imagen_opciones = pygame.transform.scale_by(imagen_opciones, 0.4)
+        x_opciones = (config.ANCHO - imagen_opciones.get_width()) // 2 + 12
         self.options_button = ButtonFactory.create_button(
             image=imagen_opciones,
-            x=275,
-            y=260,
+            x=x_opciones,
+            y=y_opciones,
             action=self.options
         )
         self.buttons.append(self.options_button)
@@ -45,10 +48,11 @@ class MainMenuState(State):
         #boton salir
         imagen_salir = self.game.resource_manager.get_image("boton_salir")
         imagen_salir = pygame.transform.scale_by(imagen_salir, 0.4)
+        x_salir = (config.ANCHO - imagen_salir.get_width()) // 2 + 4
         self.exit_button = ButtonFactory.create_button(
             image=imagen_salir,
-            x=275,
-            y=390,
+            x=x_salir,
+            y=y_salir,
             action=self.exit_game
         )
         self.buttons.append(self.exit_button)

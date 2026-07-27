55b3d52bda891f09681fb1a41a2c8808f6d5be83
serena06 <serenavargas@escuelathays.edu.ar>
2026-07-27 18:31:44 -0300

    Mejora de animaciones de patada

--- utils/redimensionar.py
+++ utils/redimensionar.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-carpeta = "assets/images/ui/items"
+carpeta = "assets/images/personajes/profe"
 
 for raiz, _, archivos in os.walk(carpeta):
     for archivo in archivos:
@@ -9,7 +9,7 @@ for raiz, _, archivos in os.walk(carpeta):
             ruta = os.path.join(raiz, archivo)
 
             imagen = Image.open(ruta)
-            imagen = imagen.resize((1000, 1200), Image.Resampling.LANCZOS)
+            imagen = imagen.resize((128, 128), Image.Resampling.LANCZOS)
             imagen.save(ruta)
 
 print("¡Todas las imágenes fueron redimensionadas!")
